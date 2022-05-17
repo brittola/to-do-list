5666b34097f27dbf8633637b50544d177c4b8509
Gabriel Rodrigues <99913525+brittola@users.noreply.github.com>
2022-05-17 11:50:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -140,6 +140,13 @@ const openEditScreen = (event) => {
 
 const confirmEdit = () => {
     getData();
+    
+    if(inEdit.value == ''){
+        alert('Experimente excluir a tarefa para não deixá-la vazia.');
+        editScreen.style.display = 'none';
+        inEdit.value = '';
+        return;
+    }
 
     tasks[taskToEditIndex].task = inEdit.value;
 
